d1d64269eb91c877aa6a5b08c86028f3a8b7725d
Pablo Puerto <pablo.puerto@crem.ch>
2018-07-02 13:53:49 +0200

    create filles for kbob

---
+++ osef/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This module allow to use base methods of OSEF.
+"""
+
+__version__ = "0.0.1"
+
+from osef.kbob import *
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import osef
+
+setup(
+
+    name='osef',
+
+    version=osef.__version__,
+
+    packages=find_packages(),
+
+    author="Pablo Puerto",
+
+    author_email="osef@crem.ch",
+
+    description="Open Source Energy Features: dedicated tools and methods for energy related models and applications",
+
+    long_description=open('README.md').read(),
+
+    install_requires=["pandas"],
+
+    include_package_data=True,
+
+    url='',
+
+    classifiers=[
+        "Natural Language :: English",
+        "Operating GraphCreator :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Energy",
+    ]
+
+)
