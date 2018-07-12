5423f6d0224013b3210b3498b920b098c113d000
diane von <diane.vongunten@crem.ch>
2018-07-12 16:39:05 +0200

    price and kbob

--- data/kbob_xls_to_csv.py
+++ data/kbob_xls_to_csv.py
@@ -1,11 +1,14 @@
 import os
 import pandas as pd
+import json
 
 
-def xls_to_csv(filename, year, filename_trans="translation_term_kbob.csv"):
+def xls_to_csv(filename, year, filename_trans="kbob_translation_term.csv"):
     """
-    Transform the kbob data from the a xlsx file to a csv. As we deal with a csv file, the comma in the french kbob
-    terminology (i.e., the last column of the xlsx file with technology names) will be transformed to a hyphen.
+    Transform the kbob data from the a xlsx file to a csv. It save the units in a separate json file.
+
+    Because we create a csv file, the comma in the french kbob terminology (i.e., the last column of the xlsx
+     file with technology names) will be transformed to a hyphen.
 
     Usage::
     * Load the kbob data in an Excel file. It is usually in a .zip file at the address :
@@ -19,35 +22,40 @@ def xls_to_csv(filename, year, filename_trans="translation_term_kbob.csv"):
     :return: none
     """
 
-    # parameters
-    column_name = ['LCA_UB', 'EP_Global', 'EP_Renew', 'EP_Nonrenew', 'EP_Renew_Onsite', 'CO2', "French_Name"]
+    # name of the files to load and create
     sheetname = "Energie Energie"
     name_base_csv = "kbob_data"
+    filename_unit = "kbob_unit" + str(year)+".json"
 
     # load kbob
     if not filename.endswith((".xls", ".xlsx", ".ods")):
         print('Error: Need an Excel file')
         return
     if os.path.isfile(filename):
-        df_kbob = pd.read_excel(filename, sheetname=sheetname, skiprows=6, usecols=[4, 5, 6, 7, 8, 9, 11])
+        df_kbob = pd.read_excel(filename, sheet_name=sheetname, skiprows=6, usecols=[4, 5, 6, 7, 8, 9, 11])
     else:
         print('Error: File not found')
         return
     if "kWh" not in filename:
         print('Warning: The kbob loaded here should be in kWh. Please check units.')
 
-    # rename column and index
+    # rename column
+    with open(filename_unit, 'r') as fp:
+        col_unit = json.load(fp)
+    column_name = col_unit.keys()
     df_kbob.dropna(inplace=True)
     df_kbob.columns = column_name
     df_kbob["French_Name"] = df_kbob["French_Name"].str.replace(',', ' -')
+
+    # get index in english terminology
     trans_data = pd.read_csv(filename_trans, header=0)
-    if (trans_data["French"].values != df_kbob["French_Name"].values).any():
+    if (trans_data["French_Name"].values != df_kbob["French_Name"].values).any():
         print('Warning: Some kbob terminology have changed : ')
-        print(df_kbob.loc[~df_kbob["French_Name"].isin(trans_data["French"].values), "French_Name"])
-    if len(trans_data["French"]) != len(df_kbob["French_Name"]):
+        print(df_kbob.loc[~df_kbob["French_Name"].isin(trans_data["French_Name"].values), "French_Name"])
+    if len(trans_data["French_Name"]) != len(df_kbob["French_Name"]):
         print('Error: Missing terminology in translation file')
         return
-    df_kbob.index = trans_data["English"]
+    df_kbob.index = trans_data["English_Name"]
 
     # save
     df_kbob.to_csv(name_base_csv + str(year) + ".csv")
---
+++ osef/access_data/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This module aims to maniplate and obtain different data such as price, kbob, etc.
+"""
+
+from osef.access_data.kbob import Kbob
+from osef.access_data.price import Price
+from osef.access_data.helper_func import find_string
